5468d1468fa515b01657f3db3718baf0074f0e46
aynig <aynigar1225@gmail.com>
2019-07-31 16:02:10 -0400

    updated

--- src/test/java/test/firstTest.java
+++ src/test/java/test/firstTest.java
@@ -4,6 +4,9 @@ public class firstTest {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("my first git project");
+
+
 
 
     }
